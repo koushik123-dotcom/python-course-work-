9a234bfb5ef59909bcf7ae49ef2a9f576b992c4a
Koushik Reddy <basika.koushik123@gmail.com>
2026-07-22 17:51:56 +0530

    Add files via upload

---
+++ Day 1/oop and pp coding.py
@@ -0,0 +1,24 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = "koushik"
+>>> print(name)
+koushik
+>>> print("hello world")
+hello world
+>>> price =500
+>>> print(price)
+500
+>>> product object
+SyntaxError: invalid syntax
+>>> object = laptop
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    object = laptop
+NameError: name 'laptop' is not defined
+>>> object = "laptop"
+>>> price = 40000
+>>> rating= 4.5
+>>> print(object,price,rating)
+laptop 40000 4.5
+>>> print(name,price)
+koushik 40000
---
+++ Day 1/types.py
@@ -0,0 +1,25 @@
+Two Types of Programming Paradigms Which Are :
+1. Procedural Programming
+2. Object-Oriented Programming (OOP)
+
+in PP
+
+login()
+select_restaurant()
+select_food()
+payment()
+confirm_order()
+
+this is is Procedural Programming because execution happens step by step.
+
+
+in OOP 
+Product Object
+Attributes:
+name
+price
+rating
+Methods:
+add_to_cart()
+buy_now()
+add_review()
